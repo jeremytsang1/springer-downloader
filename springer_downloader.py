c70d95063de30d7234b88c72e2b923e96b66ef09
Jeremy Tsang <jeremy@jeremytsang.com>
2020-04-25 16:29:03 -0700

    Create method to iterate through all search pages

--- springer_downloader.py
+++ springer_downloader.py
@@ -24,7 +24,34 @@ class SpringerDownloader():
     def __init__(self, url_suffix):
         self._URL_SUFFIX = url_suffix
 
+    def execute_on_all_pages(self, action, start_page_num=1,):
+        """Loop through each page performing action on each page."""
+        page_num = start_page_num
+        next_page_arrow_imgs = [None]  # If empty then reached the last page.
+
+        # Keep going until there is no arrow link to a following page.
+        while len(next_page_arrow_imgs) > 0:
+            print(79 * "-")
+            print(f'Processing page {page_num}')
+
+            url = (SpringerDownloader.URL_SEARCH_BASE + str(page_num)
+                   + self._URL_SUFFIX)
+
+            source = requests.get(url).text
+            soup = BeautifulSoup(source, 'lxml')
+            print(soup.title(soup)
+
+            # Find arrow button links to next page.
+            next_page_arrow_imgs = soup.main.find_all('img', alt='next')
+            page_num += 1
+
+
+def dummy_function(soup):
+    """Dummy function."""
+    return soup
+
 
 if __name__ == '__main__':
     URL_SUFFIX = '?facet-content-type=%22Book%22&package=mat-covid19_textbooks&fbclid=IwAR2dD_eYkJArztAjIwg501C7aa9sSA9FGh8ov0PCS6-eY3QFxz2NVqNanHs&facet-language=%22En%22&facet-discipline=%22Computer+Science%22'
     downloader = SpringerDownloader(URL_SUFFIX)
+    downloader.execute_on_all_pages(dummy_function)
