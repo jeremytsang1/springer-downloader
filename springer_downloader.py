ad01e217fc022739f0c77229dbbc374411bc9b39
Jeremy Tsang <jeremy@jeremytsang.com>
2020-04-25 17:08:48 -0700

    Don't hardcode the FILETYPES
    Use method to grap the keys from the DOWNLOAD_TEMPLATES instead.

--- springer_downloader.py
+++ springer_downloader.py
@@ -15,11 +15,11 @@ class SpringerDownloader():
     # CONSTANTS
     URL_BASE = 'https://link.springer.com/'
     URL_SEARCH_BASE = 'https://link.springer.com/search/page/'
-    FILETYPES = {'pdf', 'epub'}
     DOWNLOAD_TEMPLATES = {
         'epub': 'https://link.springer.com/content/pdf/10.1007%2F{}.pdf',
         'pdf': 'https://link.springer.com/download/epub/10.1007%2F{}.epub'
     }
+    FILETYPES = set(DOWNLOAD_TEMPLATES.keys())
 
     def __init__(self, url_suffix):
         self._URL_SUFFIX = url_suffix
